05e8efce53269113488d2f780e52ed514ca80851
Park Woorak <grf0503@gmail.com>
2025-05-15 19:45:38 +0900

    fix: minor changes from windows (#18)
    * fix: minor changes from windows
    
    * fix: revert adding unnecessary npm script command
    
    ---------
    
    Co-authored-by: jhlee525 <bmrcreative90@gmail.com>
    Co-authored-by: khj809 <onsealeatang@gmail.com>

--- vm/src/mlc_llm/mlc_llm_engine.cpp
+++ vm/src/mlc_llm/mlc_llm_engine.cpp
@@ -280,7 +280,7 @@ mlc_llm_engine_t::get_response_from_stream_output(
         choice.finish_reason = std::nullopt;
       }
       choice.index = static_cast<int>(i);
-      ChatCompletionMessage delta;
+
       // Size of delta_output->group_delta_token_ids Array should be 1
       const IntTuple &delta_token_ids = delta_output->group_delta_token_ids[i];
       std::vector<int32_t> delta_token_ids_vec(delta_token_ids.begin(),
@@ -290,7 +290,6 @@ mlc_llm_engine_t::get_response_from_stream_output(
         content += rstate.streamer[i]->Finish();
       }
 
-      delta.content = std::string("");
       if (template_engine_->is_botc_token(content)) {
         mode_ = output_mode::tool_call;
       } else if (template_engine_->is_eotc_token(content)) {
@@ -298,7 +297,7 @@ mlc_llm_engine_t::get_response_from_stream_output(
             template_engine_->get_json_str_if_valid(tool_call_tokens_);
         if (tool_call_json_str_opt.has_value()) {
           // if valid JSON is made, set the string as the content
-          delta.content = tool_call_json_str_opt.value();
+          choice.delta.content = tool_call_json_str_opt.value();
           choice.finish_reason = FinishReason::tool_calls;
         }
         // exit tool call mode no matter valid JSON is made
@@ -310,7 +309,7 @@ mlc_llm_engine_t::get_response_from_stream_output(
             template_engine_->get_json_str_if_valid(tool_call_tokens_);
         if (tool_call_json_str_opt.has_value()) {
           // if valid JSON is made, set the string as the content
-          delta.content = tool_call_json_str_opt.value();
+          choice.delta.content = tool_call_json_str_opt.value();
           choice.finish_reason = FinishReason::tool_calls;
           // exit tool call mode only if valid JSON is made
           mode_ = output_mode::text;
@@ -319,20 +318,22 @@ mlc_llm_engine_t::get_response_from_stream_output(
       } else if (!content.empty()) {
         // Process for reasoning outputs
         // TODO: consider other kinds of reasoning part distinguisher
-        if (content == "<think>")
+        if (content == "<think>") {
           mode_ = output_mode::reasoning;
-        else if (content == "</think>")
+          choice.delta.content = ChatCompletionMessageContent();
+        } else if (content == "</think>") {
           mode_ = output_mode::text;
-        else
-          delta.content = ChatCompletionMessageContent(content);
+          choice.delta.content = ChatCompletionMessageContent();
+        } else
+          choice.delta.content = ChatCompletionMessageContent(content);
       }
 
-      delta.role = "assistant";
-      choice.delta = delta;
+      choice.delta.role = std::string("assistant");
       if (!choice.delta.content.IsNull() || choice.finish_reason.has_value()) {
         response.choices.push_back(choice);
       }
     }
+
     // if it is not the usage block, choices cannot be empty
     if (!response.choices.empty()) {
       responses.push_back(response);
