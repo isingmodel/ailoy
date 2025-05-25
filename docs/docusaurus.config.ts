4121d7f1c7ac6caeb0466ac3bc52455f8f3b9647
grf53 <grf0503@gmail.com>
2025-05-25 21:08:31 +0900

    docs: fix `gh-pages` deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: "https://ailoy.co",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/ailoy",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
